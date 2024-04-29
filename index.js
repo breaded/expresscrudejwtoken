c95adf718bcb5013eba3a36bc808c6df0c021327
breaded <breaedX@gmail.com>
2024-04-29 10:37:35 -0400

    updated passwordless

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/login", (req,res) => {
   if (authenticatedUser(username,password)) {
     let accessToken = jwt.sign({
       data: password
-    }, 'access', { expiresIn: 60 * 60 });
+    }, 'access', { expiresIn: 60});
 
     req.session.authorization = {
       accessToken,username
@@ -92,4 +92,4 @@ const PORT =5000;
 
 app.use("/friends", routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running @ 5000"));
